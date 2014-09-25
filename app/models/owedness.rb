4b84df325637da69045dca2b6fc7bbde11bb023b
Ali Lown <ali@lown.me.uk>
2014-09-25 23:08:19 +0100

    Implement summary page

--- app/models/owedness.rb
+++ app/models/owedness.rb
@@ -1,4 +1,10 @@
 class Owedness < ActiveRecord::Base
   belongs_to :from_person, class_name: :Person
   belongs_to :to_person, class_name: :Person
+
+  def self.owes(to, from)
+    q = self.where("to_person_id = ? and from_person_id = ?", to.id, from.id)
+    return q[0]["amount"] if q.size > 0
+    return 0
+  end
 end

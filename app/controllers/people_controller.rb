c65f301e5a076d2070c074aea88da600dab298a8
Ali Lown <ali@lown.me.uk>
2014-09-25 19:55:36 +0100

    Don't let people delete other people

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -61,25 +61,13 @@ class PeopleController < ApplicationController
     @person = Person.find(params[:id])
 
     respond_to do |format|
-      if @person.update_attributes(person_params)
+      if @person == current_person && @person.update_attributes(person_params)
         format.html { redirect_to @person, notice: 'Person was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
         format.json { render json: @person.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # DELETE /people/1
-  # DELETE /people/1.json
-  def destroy
-    @person = Person.find(params[:id])
-    @person.destroy
-
-    respond_to do |format|
-      format.html { redirect_to people_url }
-      format.json { head :no_content }
     end
   end
 

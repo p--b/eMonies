4f684ae35a8fdcf3e53e89416d743925ffa07083
Ali Lown <ali@lown.me.uk>
2014-09-25 14:38:49 +0100

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EMonies::Application.config.secret_token = '5679541d68aea4b15975cc54990d5297bd77dbff1378d6c2cf64ccecba82c56a91e1b517e203a6b9d2cefe6ef3b24bb1465e4e2b5e9be39d254dcf72e661a52c'
